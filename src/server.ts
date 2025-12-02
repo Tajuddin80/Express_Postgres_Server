f004fe81b77d3b6db508c1b4a80ad4b7546016cd
Tajuddin-green <cmtajuddinchowdhury@gmail.com>
2025-12-02 23:46:54 +0600

    user table actions completed

--- src/server.ts
+++ src/server.ts
@@ -42,7 +42,7 @@ const initDB = async () => {
 initDB();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world and humans");
+  res.send("Hello world");
 });
 
 app.post("/create-user", async (req: Request, res: Response) => {
@@ -68,6 +68,114 @@ app.post("/create-user", async (req: Request, res: Response) => {
     res.status(500).json({
       success: false,
       message: "Error while user creation",
+    });
+  }
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
+    res.status(200).json({
+      success: true,
+      message: "users retrieved successfully",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Error while all users get",
+    });
+  }
+});
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params?.id;
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "user retrieved successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Error while single user get",
+    });
+  }
+});
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const { name, email, age, phone, address } = req.body;
+
+    const result = await pool.query(
+      `
+      UPDATE users 
+      SET name = $1, email =$2, age = $3, phone = $4, address = $5, updated_at = NOW()
+      WHERE id = $6
+      RETURNING *;
+      `,
+      [name, email, age, phone, address, id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User updated successfully",
+        data: result.rows[0],
+      });
+    }
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Error while updating user",
+    });
+  }
+});
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const result = await pool.query(
+      `DELETE FROM users
+       WHERE id = $1`,
+      [id]
+    );
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User deleted successfully",
+        data: result.rows,
+      });
+    }
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Error while deleting user",
     });
   }
 });
